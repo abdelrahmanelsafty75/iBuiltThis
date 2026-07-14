3f8e4c9dbef153dc7443840958daad759200491a
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-07-14 15:18:50 +0300

    chore: setup drizzle ORM and neon DB with mock data too

---
+++ db/index.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema';
+
+config({ path: ".env.local" }); 
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is explicitly missing in environment variables.");
+}
+
+export const db = drizzle(process.env.DATABASE_URL, { schema });
---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is explicitly missing.");
+}
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
+
